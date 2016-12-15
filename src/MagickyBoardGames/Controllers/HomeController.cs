cf595dd94be3f3674db5583b77123f1478ea727e
William C. Chronowski <wchronowski@pekininsurance.com>
2016-12-15 15:43:25 -0600

    Removed unnecessary ui

--- src/MagickyBoardGames/Controllers/HomeController.cs
+++ src/MagickyBoardGames/Controllers/HomeController.cs
@@ -6,18 +6,6 @@ namespace MagickyBoardGames.Controllers {
             return View();
         }
 
-        public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact() {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error() {
             return View();
         }
