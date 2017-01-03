2e84be5627f4aed1efae18aea075d1f029c2dde1
William C. Chronowski <wchronowski@pekininsurance.com>
2017-01-02 18:11:35 -0600

    Add Game View Model

---
+++ src/MagickyBoardGames/ViewModels/GameViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagickyBoardGames.ViewModels
+{
+    public class GameViewModel : IViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public string PlayerRange { get; set; }
+    }
+}
