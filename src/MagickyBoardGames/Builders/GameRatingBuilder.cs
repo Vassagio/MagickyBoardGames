52ad7cd42c95548d27e9a1fdd10fc4e2a97ada10
William C. Chronowski <wchronowski@pekininsurance.com>
2017-01-17 14:59:16 -0600

    Add ratings to player view

---
+++ src/MagickyBoardGames/Builders/GameRatingBuilder.cs
@@ -0,0 +1,44 @@
+﻿using MagickyBoardGames.Models;
+using MagickyBoardGames.Repositories;
+using MagickyBoardGames.ViewModels;
+
+namespace MagickyBoardGames.Builders {
+    public class PlayerRatingBuilder : IBuilder<GamePlayerRating, PlayerRatingViewModel> {
+        private readonly IRatingRepository _ratingRepository;
+        private readonly IBuilder<Rating, RatingViewModel> _ratingBuilder;
+        private readonly IUserRepository _userRepository;
+        private string _playerName;
+        private RatingViewModel _rating;
+
+        public PlayerRatingBuilder(IRatingRepository ratingRepository, IBuilder<Rating, RatingViewModel> ratingBuilder, IUserRepository userRepository) {
+            _ratingRepository = ratingRepository;
+            _ratingBuilder = ratingBuilder;
+            _userRepository = userRepository;
+        }
+
+        public PlayerRatingViewModel ToViewModel() {
+            return new PlayerRatingViewModel {
+                PlayerName = _playerName,
+                Rating = _rating
+            };
+        }
+
+        public PlayerRatingViewModel Build(GamePlayerRating entity) {
+            var player = _userRepository.GetById(entity.PlayerId).Result;
+            var rating = _ratingRepository.GetBy(entity.RatingId).Result;
+            if (player == null || rating == null)
+                return new PlayerRatingViewModel();
+            _playerName = player.UserName;
+            _rating = _ratingBuilder.Build(rating);
+            return ToViewModel();
+        }
+
+        public GamePlayerRating ToEntity() {
+            return new GamePlayerRating();
+        }
+
+        public GamePlayerRating Build(PlayerRatingViewModel viewModel) {
+            return ToEntity();
+        }
+    }
+}
---
+++ src/MagickyBoardGames/ViewModels/GameRatingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagickyBoardGames.ViewModels {
+    public class GameRatingViewModel : IViewModel {
+        [Display(Name = "Game Name")]
+        public string GameName { get; set; }
+
+        public RatingViewModel Rating { get; set; }
+    }
+}
